3119dbb9d47062d4ec500b8eaecded3888a42dd4
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-03-15 10:53:28 -0600

    Main Sources

---
+++ src/assets/images.jsx
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import intro from './illustration-intro.svg';
+import avatarAli from './avatar-ali.png';
+import avatarAnisha from './avatar-anisha.png';
+import avatarRichard from './avatar-richard.png';
+import avatarShanai from './avatar-shanai.png';
+import bgSimpDesk from './bg-simplify-section-desktop.svg';
+import bgSimpMobile from './bg-simplify-section-mobile.svg';
+import bgTablePat from './bg-tablet-pattern.svg';
+import close from './icon-close.svg';
+import ham from './icon-hamburger.svg';
+
+const images = {
+    logo,
+    intro,
+    avatarAli,
+    avatarAnisha,
+    avatarRichard,
+    avatarShanai,
+    bgSimpDesk,
+    bgSimpMobile,
+    bgTablePat,
+    close,
+    ham,
+}
+
+export default images;
